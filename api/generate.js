fe48c1a949d1aad5ac400d19918d8a4244dd74d2
GusBroadberry1 <willyb0606@icloud.com>
2026-06-20 14:01:44 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -21,6 +21,7 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
+    console.log("Anthropic response:", JSON.stringify(data));
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
