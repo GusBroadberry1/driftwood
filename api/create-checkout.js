0b807005063c83600b29f3aaf0ebd71060060d30
GusBroadberry1 <willyb0606@icloud.com>
2026-06-23 11:31:26 +0100

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,4 +1,5 @@
 import Stripe from "stripe";
+console.log("KEY EXISTS:", !!process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
